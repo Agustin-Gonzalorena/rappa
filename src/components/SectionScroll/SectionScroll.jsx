09ce402c60d038d949e0257d4486f93c663c7783
Agustin-Gonzalorena <agonzalorena@hotmail.com>
2023-12-07 20:42:07 -0300

    update

--- src/components/SectionScroll/SectionScroll.jsx
+++ src/components/SectionScroll/SectionScroll.jsx
@@ -2,36 +2,25 @@
 import styles from "./SectionScroll.module.css";
 import { useEffect, useRef, useState } from "react";
 const SectionScroll = ({ data }) => {
+  const sectionRefs = useRef([]);
   const [sectNumber, setSectNumber] = useState(1);
-  const section1 = useRef(null);
-  const section2 = useRef(null);
-  const section3 = useRef(null);
-  // ...
+
+  useEffect(() => {
+    sectionRefs.current = sectionRefs.current.slice(0, data.length);
+  }, [data]);
 
   useEffect(() => {
     const handleScroll = () => {
       let sumDistance = 200;
-      if (section1.current && section2.current && section3.current) {
-        const section1Rect = section1.current.getBoundingClientRect();
-        const section2Rect = section2.current.getBoundingClientRect();
-        const section3Rect = section3.current.getBoundingClientRect();
-        if (
-          section1Rect.top - sumDistance >= 0 &&
-          section1Rect.bottom <= window.innerHeight
-        ) {
-          setSectNumber(1);
-        } else if (
-          section2Rect.top - sumDistance >= 0 &&
-          section2Rect.bottom <= window.innerHeight
-        ) {
-          setSectNumber(2);
-        } else if (
-          section3Rect.top - sumDistance >= 0 &&
-          section3Rect.bottom <= window.innerHeight
-        ) {
-          setSectNumber(3);
+      const sectionRects = sectionRefs.current.map((ref) =>
+        ref.current.getBoundingClientRect()
+      );
+
+      sectionRects.forEach((rect, index) => {
+        if (rect.top - sumDistance >= 0 && rect.bottom <= window.innerHeight) {
+          setSectNumber(index + 1);
         }
-      }
+      });
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -40,6 +29,7 @@ const SectionScroll = ({ data }) => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
     <>
       <div className={styles.boxTitle}>
@@ -49,49 +39,38 @@ const SectionScroll = ({ data }) => {
         <div className={styles.left}>
           <div className={styles.boxImg}>
             <div className={styles.imgContain}>
-              <img
-                src={data[0].img}
-                alt={data[0].title}
-                className={styles.images}
-                style={sectNumber == 1 ? { opacity: 1 } : { opacity: 0 }}
-              />
-              <img
-                src={data[1].img}
-                alt={data[1].title}
-                className={styles.images}
-                style={sectNumber == 2 ? { opacity: 1 } : { opacity: 0 }}
-              />
-              <img
-                src={data[2].img}
-                alt={data[2].title}
-                className={styles.images}
-                style={sectNumber == 3 ? { opacity: 1 } : { opacity: 0 }}
-              />
+              {data.map((item, index) => {
+                return (
+                  <img
+                    key={item.id}
+                    src={item.img}
+                    alt={item.title}
+                    className={styles.images}
+                    style={
+                      sectNumber === index + 1 ? { opacity: 1 } : { opacity: 0 }
+                    }
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
         <div className={styles.boxRight}>
-          <div
-            style={sectNumber == 1 ? { opacity: 1 } : { opacity: 0 }}
-            className={styles.boxText}
-          >
-            <h3>{data[0].title}</h3>
-            <div ref={section1}>{data[0].text}</div>
-          </div>
-          <div
-            style={sectNumber == 2 ? { opacity: 1 } : { opacity: 0 }}
-            className={styles.boxText}
-          >
-            <h3>{data[1].title}</h3>
-            <div ref={section2}>{data[1].text}</div>
-          </div>
-          <div
-            style={sectNumber == 3 ? { opacity: 1 } : { opacity: 0 }}
-            className={styles.boxText}
-          >
-            <h3>{data[2].title}</h3>
-            <div ref={section3}>{data[2].text}</div>
-          </div>
+          {data.map((item, index) => {
+            sectionRefs.current[index] = useRef(null);
+            return (
+              <div
+                key={item.id}
+                style={
+                  sectNumber === index + 1 ? { opacity: 1 } : { opacity: 0 }
+                }
+                className={styles.boxText}
+              >
+                <h3>{item.title}</h3>
+                <div ref={sectionRefs.current[index]}>{item.text}</div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
